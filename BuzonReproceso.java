fe6be63e92bf0a4223881a28d9cd8f6a053c533c
davmora04 <d.morar@uniandes.edu.co>
2025-02-13 10:36:09 -0500

    control logs BuzonReproceso

--- BuzonReproceso.java
+++ BuzonReproceso.java
@@ -8,20 +8,37 @@ public class BuzonReproceso {
     public synchronized void depositar(Product p) {
         cola.add(p);
         notifyAll(); // Avisar a los hilos que puedan estar esperando retirar
+        mostrarEstado();
     }
 
     /**
-     * Método no bloqueante para que el productor verifique sin quedar esperando
-     * (por si deseas mantener la 'prioridad' de reproceso de forma rápida).
+     * Método no bloqueante para que el productor verifique sin quedar esperando.
      */
     public synchronized Product retirar() {
         if (cola.isEmpty()) {
             return null;
         }
-        return cola.remove();
+        Product p = cola.remove();
+        notifyAll();
+        mostrarEstado();
+        return p;
     }
 
     public synchronized boolean estaVacio() {
         return cola.isEmpty();
+    }
+    
+    /**
+     * Muestra el estado actual del buzón de reproceso solo si no está vacío.
+     */
+    private void mostrarEstado() {
+        if (!cola.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("BuzonReproceso actual: ");
+            for (Product prod : cola) {
+                sb.append(prod.toString()).append(" ");
+            }
+            System.out.println(sb.toString());
+        }
     }
 }
